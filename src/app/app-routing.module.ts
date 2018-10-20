f26549ad1de446ffc19f4fd53de70bd7ace53fa6
Michael Bergman <michael@simutek.se>
2018-10-20 15:29:46 +0200

    Added module as lazy loaded and a budget

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: AppComponent,
+    children: [
+      {
+        path: 'customers',
+        loadChildren: './customers/customers.module#CustomersModule'
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
